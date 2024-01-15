d11c86ea34b0c4c1bf8ad2b5d0a503efa1ad8071
Juancho-Coding <juanpabloalvarezovalle@gmail.com>
2024-01-15 12:17:22 -0500

    Added better animation to highlight the section in navbar

--- src/components/LinkItem.tsx
+++ src/components/LinkItem.tsx
@@ -4,15 +4,15 @@ import classes from "./LinkItem.module.css";
 
 const LinkItem = ({ to, text, location }: LinkItemProps) => {
     // if the link is clicked change its color
-    const selected = location.includes(text) ? "#cfcfcf" : "white";
+    const selected = location.includes(text);
     return (
         <a href={to} className={classes.link}>
             <Typography
                 sx={{
-                    color: selected,
                     fontSize: { xs: "0.9rem", sm: "0.9rem", md: "1.1rem" },
-                    "&:hover": { color: "#cfcfcf" },
+                    "&:hover": { color: "#9c9c9c" },
                 }}
+                className={`${classes.text} ${selected ? classes["text-selected"] : ""}`}
             >
                 {text}
             </Typography>
