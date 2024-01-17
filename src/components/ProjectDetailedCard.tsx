52c21c63e1fb74dcf6e515b259c825204e6d2d66
Juancho-Coding <juanpabloalvarezovalle@gmail.com>
2024-01-17 13:50:47 -0500

    Added a card for a detailed project information and a container to show projectcard and interact with detailedProjectCard

---
+++ src/components/ProjectDetailedCard.tsx
@@ -0,0 +1,107 @@
+import { Box, Button, Card, Fade, Typography } from "@mui/material";
+import { ProjectInformation } from "../assets/information";
+import { useState } from "react";
+import Markdown from "react-markdown";
+
+import GitHubIcon from "@mui/icons-material/GitHub";
+import PreviewIcon from "@mui/icons-material/Preview";
+
+const ProjectDetailedCard = ({ onClose, item }: props) => {
+    const [visibility, setVisibility] = useState(true);
+
+    return (
+        <Fade
+            in={visibility}
+            appear={true}
+            timeout={{ enter: 500, exit: 500 }}
+            onExited={() => {
+                onClose();
+            }}
+        >
+            <Card
+                sx={{
+                    boxSizing: "border-box",
+                    width: "600px",
+                    height: "500px",
+                    padding: "10px",
+                    position: "relative",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                {item && (
+                    <>
+                        <Box
+                            sx={{
+                                boxSizing: "border-box",
+                                display: "flex",
+                                flexDirection: "column",
+                            }}
+                        >
+                            <Box>
+                                <Typography sx={{ color: "black" }} variant="h5">
+                                    {item.title}
+                                </Typography>
+                            </Box>
+                            <Box>
+                                <Typography sx={{ color: "black", fontSize: "1rem" }}>
+                                    <Markdown>{item.description}</Markdown>
+                                </Typography>
+                            </Box>
+                            <Box display="flex" justifyContent="center">
+                                {item.demo.length !== 0 && (
+                                    <Button
+                                        variant="contained"
+                                        disabled
+                                        startIcon={<PreviewIcon />}
+                                    >
+                                        Live Preview
+                                    </Button>
+                                )}
+                                {item.github.length !== 0 && (
+                                    <Button
+                                        variant="contained"
+                                        disabled
+                                        startIcon={<GitHubIcon />}
+                                        sx={{ ml: 2 }}
+                                    >
+                                        Code
+                                    </Button>
+                                )}
+                            </Box>
+                        </Box>
+                    </>
+                )}
+                {!item && (
+                    <Box>
+                        <Typography sx={{ color: "black" }} variant="h5">
+                            Choose a project from the right to visualize
+                        </Typography>
+                    </Box>
+                )}
+                <Button
+                    color="secondary"
+                    variant="contained"
+                    sx={{
+                        position: "absolute",
+                        top: 10,
+                        right: 10,
+                    }}
+                    onClick={() => {
+                        setVisibility(false);
+                    }}
+                >
+                    Close
+                </Button>
+            </Card>
+        </Fade>
+    );
+};
+
+type props = {
+    onClose: () => void;
+    item: ProjectInformation | null;
+};
+
+export default ProjectDetailedCard;
---
+++ src/components/ProjectsContainer.tsx
@@ -0,0 +1,91 @@
+import { Box, Typography } from "@mui/material";
+import { useState } from "react";
+import ProjectCard from "./ProjectCard";
+
+import { projects } from "./../assets/information";
+
+import ProjectDetailedCard from "./ProjectDetailedCard";
+
+const ProjectsContainer = (props: props) => {
+    const [detailedView, setDetailedView] = useState(false);
+    const [detailedItem, setDetailedItem] = useState<number | null>(null);
+
+    const cardClickHandle = (_event: React.MouseEvent<HTMLElement, MouseEvent>, key: string) => {
+        setDetailedView(true);
+        const clickedItem = projects.findIndex((element) => {
+            return element.id === key;
+        });
+        setDetailedItem(clickedItem);
+        props.onSelection();
+    };
+
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "row",
+                boxSizing: "border-box",
+                justifyContent: "center",
+                columnGap: 5,
+            }}
+        >
+            <Box
+                sx={{
+                    margin: "20px",
+                    boxSizing: "border-box",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "stretch",
+                }}
+            >
+                <Typography
+                    sx={{
+                        fontSize: "1.1rem",
+                        textAlign: "center",
+                        pb: "10px",
+                    }}
+                >
+                    Here you will find some projects I've done <br />
+                    during my career, some about software development, <br />
+                    others about electronics, go ahead and click one
+                </Typography>
+
+                <Box
+                    sx={{
+                        justifyItems: "center",
+                        boxSizing: "border-box",
+                        display: "grid",
+                        gridTemplateColumns: "1fr 1fr",
+                        gridTemplateRows: "1fr 1fr",
+                        rowGap: "20px",
+                        columnGap: "20px",
+                    }}
+                >
+                    {projects.map((element) => {
+                        return (
+                            <ProjectCard
+                                key={element.id}
+                                onClick={cardClickHandle}
+                                element={element}
+                            />
+                        );
+                    })}
+                </Box>
+            </Box>
+
+            {detailedView && (
+                <ProjectDetailedCard
+                    item={detailedItem !== null ? projects[detailedItem] : null}
+                    onClose={() => setDetailedView(false)}
+                />
+            )}
+        </Box>
+    );
+};
+
+type props = {
+    onSelection: () => void;
+};
+
+export default ProjectsContainer;
