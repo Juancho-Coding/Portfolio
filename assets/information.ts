a6e6cc26ddff2e78dcf965c76adb3b3bb9162c2a
juan_pablo_alvarez <juanpabloalvarezovalle@gmail.com>
2026-08-01 19:10:09 -0500

    Updates

--- assets/information.ts
+++ assets/information.ts
@@ -1,86 +1,48 @@
 export type ProjectInformation = {
-    title: string;
-    description: string;
-    image: string;
-    id: string;
-    github: string;
-    demo: string;
-};
+  title: string
+  description: string
+  image: string
+  id: string
+  github: string
+  demo: string
+  page: string
+}
 
 export const projects: ProjectInformation[] = [
-    {
-        title: "Calibration bench for domestic water metters",
-        /* description: `Software que Desarrolle un programa que no solo permite la calibración
-         de medidores de agua, sino que también guía al operario de manera integral durante todo el proceso
-        . La clave de nuestro proyecto reside en el desarrollo cuidadoso de esta herramienta, que automatiza
-        la creación de documentos PDF detallados y la gestión eficiente del almacenamiento de resultados en 
-        una sólida base de datos`, */
-        description: `
-This is a Standalone software for executing calibration tests on residential water meters. It has two sections,
-the first allows administrative tasks and data entry, while the second interacts with hardware elements to perform 
-the necessary tests on the meters. Once the tests are completed, results are registered in a PDF document format.
-
-Additionally, it communicates with a database to store metrics and results of each test.
-        
-> **Stack (Prior transition)** LabVIEW, PostgresQL, SQLite, .NET
-
-> **Stack (During transition)** Frontend: React, Electron.
-> Backend: NodeJS, Typescript, PostgreSQL, TypeORM, Python
-
-#### *This project is currently being rewritten from LabVIEW to Web frameworks (Typescript, NodeJS, React)*
-
-`,
-        image: "project1_thumb.jpg",
-        id: "1",
-        github: "",
-        demo: "",
-    },
-    {
-        title: "ChatGPT Clone",
-
-        description: `
-Experience the power of a ChatGPT-inspired project! Unlock a world of possibilities by registering on the platform
-and diving into the chat. Ask any general questions or request code snippets effortlessly. Your journey begins now –
-**sign up** and explore without sharing personal or sensitive information. Elevate your experience with our feature-rich
-platform!
-
-> - Register and login with credentials validation using JWT
-> - Public and protected routes in NodeJS with ExpressJS
-> - Use of mongoDB and mongoose to store user chats
-> - Extensive use of REST APIs and openAI API
-
-> **Stack** Frontend: React, MUI.
-> Backend: NodeJS, Typescript, MongoDB, Mongoose
-`,
-        image: "chatclone_thumb.jpg",
-        id: "2",
-        github: "https://github.com/Juancho-Coding/Chatbot-clone",
-        demo: "",
-    },
-    {
-        title: "Cleaning folder CLI",
-        description: `
-This is a simple command line interface that helps us to delete the
-unwanted folder of the projects developed with NodeJS when were are
-making backups, AKA **node_modules**
-
-> **Stack** Pure NodeJS
-`,
-        image: "cli_thumb.jpg",
-        id: "3",
-        github: "https://github.com/Juancho-Coding/Utility-backup-cli",
-        demo: "",
-    },
-    {
-        title: "This Portfolio 😎",
-        description: `
-If you like this portfolio, feel free to check the code and use as you want.
-This page is mainly built with React an MUI, in the github page you will 
-find the instructions to clone the repository and
-run it in a local environment`,
-        image: "portfolio_thumb.jpg",
-        id: "4",
-        github: "https://github.com/Juancho-Coding/Portfolio",
-        demo: "",
-    },
-];
+  {
+    title: 'project_iot',
+    description: `project_iot_description`,
+    image: 'project-iot/iotfrontpage.webp',
+    id: '1',
+    github: '',
+    demo: 'https://juanchocoding.dev/',
+    page: '/#/projectDetails/IotSystem',
+  },
+  {
+    title: 'project_calbench_title',
+    description: `project_calbench_description`,
+    image: 'project-calibration/bench.webp',
+    id: '2',
+    github: '',
+    demo: '',
+    page: '/#/projectDetails/calibration_Meters',
+  },
+  {
+    title: 'project_dashboard_title',
+    description: `project_dashboard_description`,
+    image: 'project-management/managementfrontpage.webp',
+    id: '3',
+    github: '',
+    demo: '',
+    page: '/#/projectDetails/managementLab',
+  },
+  // {
+  //   title: 'project_cinema_title',
+  //   description: `project_cinema_description`,
+  //   image: 'project-cinema/cinemafrontpage.webp',
+  //   id: '4',
+  //   github: 'https://github.com/Juancho-Coding/movie-theater',
+  //   demo: 'https://movie-theater-87qg.vercel.app/',
+  //   page: '',
+  // },
+]
